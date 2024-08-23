98173de4313a9e233bf180b37652e7f744b526a3
mehdi-chebbi <mehdi.chebbi@esprit.tn>
2024-08-23 09:09:22 +0100

    last v2

--- OSS-FILTERED-tunisia/Get_L2A_IDS_Repeating.py
+++ OSS-FILTERED-tunisia/Get_L2A_IDS_Repeating.py
@@ -6,66 +6,61 @@ def fetch_data(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return None
 
-def load_existing_ids(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            return set(line.strip().split(": ")[1] for line in file if line.startswith("Product ID:"))
-    except FileNotFoundError:
-        return set()
+def load_last_fetched_id(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as file:
+            return file.readline().strip()
+    return None
 
-def filter_new_ids(products, existing_ids):
-    new_products = []
-    for product in products:
-        product_id = product.get('Id')
-        if product_id and product_id not in existing_ids:
-            new_products.append(product)
-    return new_products
+def save_last_fetched_id(last_id, file_name):
+    with open(file_name, 'w') as file:
+        file.write(last_id)
 
 def save_product_ids(products, file):
     for product in products:
         product_id = product.get('Id')
-        file.write(f"Product ID: {product_id}\n")
+        if product_id:
+            file.write(f"Product ID: {product_id}\n")
+            print(f"New Product ID: {product_id}")
 
 today = datetime.now(timezone.utc).date()
-start_date = today - timedelta(days=5)
+start_date = today - timedelta(days=1)
 start_date_str = start_date.strftime('%Y-%m-%dT00:00:00.000Z')
 end_date_str = today.strftime('%Y-%m-%dT23:59:59.999Z')
 
 url = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=((ContentDate/Start%20ge%20{start_date_str}%20and%20ContentDate/Start%20le%20{end_date_str})%20and%20(Online%20eq%20true)%20and%20(OData.CSC.Intersects(Footprint=geography%27SRID=4326;POLYGON%20((7.522749%2030.230347,%2011.611239%2030.230347,%2011.611239%2037.6320274,%207.522749%2037.6320274,%207.522749%2030.230347))%27))%20and%20(((((Collection/Name%20eq%20%27SENTINEL-2%27)%20and%20(((Attributes/OData.CSC.StringAttribute/any(i0:i0/Name%20eq%20%27productType%27%20and%20i0/Value%20eq%20%27S2MSI2A%27)))))))))&$expand=Attributes&$expand=Assets&$orderby=ContentDate/Start%20asc&$top=1000"
 
-file_path = 'L2A-IDS'
-temp_file_path = 'L2A-IDS.tmp'
+last_fetched_id_file = 'last_fetched_id_L2A.txt'
+last_fetched_id = load_last_fetched_id(last_fetched_id_file)
 
-# Load existing product IDs from the file
-existing_ids = load_existing_ids(file_path)
+new_ids_found = False
 
-# Fetch and filter new product IDs
-with open(temp_file_path, 'w') as temp_file:
+with open('L2A-IDS-Repeating', 'a') as file:
     while url:
         data = fetch_data(url)
-
         if data is None:
             break
 
         products = data.get('value', [])
-        
-        # Filter out products that are already in the file
-        new_products = filter_new_ids(products, existing_ids)
-        
-        # Save only the new, non-duplicate products to the temporary file
-        save_product_ids(new_products, temp_file)
+        for product in products:
+            product_id = product.get('Id')
+            if product_id == last_fetched_id:
+                url = None  # Stop the loop if the last fetched ID is found
+                break
+            if not last_fetched_id:
+                new_ids_found = True
+            save_product_ids([product], file)
+            last_fetched_id = product_id  # Update last fetched ID
 
         url = data.get('@odata.nextLink')
 
-# Replace the old file with the new file
-if os.path.exists(file_path):
-    os.remove(file_path)
-os.rename(temp_file_path, file_path)
-
-print("Data saved to 'L2A-IDS' file.")
+if new_ids_found:
+    save_last_fetched_id(last_fetched_id, last_fetched_id_file)
+    print("New data saved.")
+else:
+    print("No new product IDs found today.")
--- OSS-FILTERED-tunisia_jendouba/Download-GRD-L2A-DATA.sh
+++ OSS-FILTERED-tunisia_jendouba/Download-GRD-L2A-DATA.sh
@@ -19,3 +19,11 @@ rm -rf "/mnt/c/Users/mehdi/Desktop/OSS-FILTERED/Products/L2A-Products"/*
 
 ./Tunisia_jendouba_GRD_IW.sh
 ./Tunisia_jendouba_L2A.sh
+
+# Unzip files in L2A and GRD-IW product directories
+find products/L2A-Products/ -type f -name "*.zip" -exec unzip -d products/L2A-Products/ {} \;
+find products/GRD-IW-Products/ -type f -name "*.zip" -exec unzip -d products/GRD-IW-Products/ {} \;
+
+# Remove all zip files
+find products/L2A-Products/ -type f -name "*.zip" -delete
+find products/GRD-IW-Products/ -type f -name "*.zip" -delete
